f323583d9bde1c315005c101c112ec6d7afa8e01
Taylor <taylor@baremetalcloud.com>
2021-02-19 02:19:15 -0500

    Added react client.

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Uploader from './components/uploader';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CSV Upload</h1>
+        <Uploader/>
+    </div>
+  );
+}
+
+export default App;
