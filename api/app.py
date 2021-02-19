d098ccb815c1acc6efe581101561184f27e8cf47
Taylor <taylor@baremetalcloud.com>
2021-02-19 04:51:33 -0500

    Add csv validation to post request

--- api/app.py
+++ api/app.py
@@ -1,9 +1,8 @@
-import os
-
 import flask_cors
-from flask import Flask, flash, request, redirect, url_for, session
+from flask import Flask, request
+from flask_cors import cross_origin
 from werkzeug.utils import secure_filename
-from flask_cors import CORS, cross_origin
+import csv
 import logging
 
 logging.basicConfig(level=logging.INFO)
@@ -17,21 +16,45 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
-
-
 @app.route('/upload', methods=['POST'])
+@cross_origin()
 def upload_file():
     target = UPLOAD_FOLDER
     file = request.files['file']
     filename = secure_filename(file.filename)
-    destination="/".join([target, filename])
+    destination = "/".join([target, filename])
+
+    # If cell check returns false there is a empty cell
+    if not csv_cell_checker(filename):
+        return 'There is an empty cell in the file'
+    # If column and row check returns false the rows and columns do not match what's expected
+    if not csv_col_row_check(filename):
+        return 'CSV file dose not match format of 3 Columns and 10 Rows'
+
     file.save(destination)
-    response="File Uploaded"
+    response = "File Uploaded Successfully"
     return response
 
+
+# Helper function to check if there are any empty cells in the csv file.
+def csv_cell_checker(file):
+    with open(str(file), 'r') as csv_file:
+        for row in csv.reader(csv_file):
+            if '' in row:
+                print('empty cell')
+                return False
+    return True
+
+
+# Helper function check the number of columns and rows in the uploaded csv file
+def csv_col_row_check(file):
+    with open(str(file), 'r') as csv_file:
+        columns = csv_file.readline()
+        rows = sum(1 for line in csv_file) + 1
+    if columns == 3 and rows == 10:
+        return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
--- client/src/components/uploader.js
+++ client/src/components/uploader.js
@@ -4,38 +4,34 @@ class Uploader extends React.Component {
       constructor(props) {
     super(props);
 
-    this.state = {
-      imageURL: '',
-    };
 
-    this.handleUploadImage = this.handleUploadImage.bind(this);
+    this.handleUpload = this.handleUpload.bind(this);
   }
 
-  handleUploadImage(ev) {
+  handleUpload(ev) {
     ev.preventDefault();
 
     const data = new FormData();
     data.append('file', this.uploadInput.files[0]);
-    data.append('filename', this.fileName.value);
 
     fetch('http://localhost:5000/upload', {
       method: 'POST',
       body: data,
     }).then((response) => {
-      response.json().then((body) => {
-        this.setState({ csvURL: `http://localhost:5000/${body.file}` });
-      });
+        if (response.status === 200){
+            alert("File Uploaded Successfully");
+        }
+        else {
+            alert("Error Uploading file")
+        }
     });
   }
 
   render() {
     return (
-      <form onSubmit={this.handleUploadImage}>
+      <form onSubmit={this.handleUpload}>
         <div>
           <input ref={(ref) => { this.uploadInput = ref; }} type="file" />
-        </div>
-        <div>
-          <input ref={(ref) => { this.fileName = ref; }} type="text" placeholder="Enter the desired name of file" />
         </div>
         <br />
         <div>
